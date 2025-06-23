c64ed45f96fde1699453b5027326631e7c1537d3
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2025-06-23 12:34:21 -0300

    Primeiro commit

---
+++ src/main/java/com/dougFSilva/clubesenai/ClubesenaiApplication.java
@@ -0,0 +1,13 @@
+package com.dougFSilva.clubesenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubesenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubesenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dougFSilva/clubesenai/ClubesenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougFSilva.clubesenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubesenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
